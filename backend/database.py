0b2c3960f3e02eea15bd847d64d8fda79478992a
EscuderoEric <escuderic4@gmail.com>
2025-09-16 14:18:32 -0400

    Added IMG upload

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .database import engine
 import os
 
 # Database URL from environment variable or default
